032d4f90c7c1bd869ed2cffad8fd0aac7d58fad9
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-07-25 17:56:36 +0530

    Completed with Variables

---
+++ Variables/AddByteData.java
@@ -0,0 +1,10 @@
+
+public class AddByteData {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+        byte c = (byte) (a + b);// byte c=a+b;//Compile Time Error: because a+b=20 will be int
+
+        System.out.println(c);
+    }
+}
