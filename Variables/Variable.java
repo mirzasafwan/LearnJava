4540b1e6b8336a3e0e58e5e711ca7fb0c1cfe6a4
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-07-26 15:52:35 +0530

    Variables Compile time and Run Time

---
+++ Variables/Variable.java
@@ -0,0 +1,13 @@
+class Variable {
+    int a;
+
+    // void get() {
+    // a = 10;
+    // System.out.println(a);
+    // }
+
+    public static void main(String arg[]) {
+        Variable a = new Variable();
+        a.a = 10;
+    }
+}
