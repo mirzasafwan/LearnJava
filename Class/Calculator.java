b9739c5ee7d981c0f47da2f225ff84e0dc211a75
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-03 21:17:51 +0530

    Calculator updated Program

--- Class/Calculator.java
+++ Class/Calculator.java
@@ -1,32 +1,69 @@
-public class Calculator {
-    static int a = 6;
-    static int b = 2;
+// public class Calculator {
 
-    static int add() {
-        return a + b;
+// static int add(int a, int b) {
+// return a + b;
+// }
+
+// static int sub(int a, int b) {
+// return a - b;
+// }
+
+// static int mul(int a, int b) {
+// return a * b;
+// }
+
+// static int div(int a, int b) {
+// return a / b;
+// }
+
+// public static void main(String[] args) {
+// System.out.println(
+// " a + b = " + add(10, 20) + "\n a - b = " + sub(20, 10)
+// + "\n a * b = "
+// + mul(2, 3)
+// + "\n a / b = " + div(6, 2));
+// }
+// }
+
+class Calculator {
+    int a = m1();
+    static int b = m2();
+    {
+        System.out.println("Instance Initialize Block");//
+    }
+    static {
+        System.out.println("Static block");//
     }
 
-    static int sub() {
-        return a - b;
+    int m1() {
+        System.out.println("Non Static Method");//
+        return 1;
     }
 
-    static int mul() {
-        return a * b;
-    }
-
-    static int div() {
-        return a / b;
-    }
-
-    static void result() {
-        System.out.println(
-                " a + b = " + add() + "\n a - b = " + sub()
-                        + "\n a * b = "
-                        + mul()
-                        + "\n a / b = " + div());
+    static int m2() {
+        System.out.println("Static Method");//
+        return 1;
     }
 
     public static void main(String[] args) {
-        result();
+        Calculator obj = new Calculator();
+
     }
 }
+
+// class Calculator {
+// int id;
+
+// Calculator(int a, int b) {
+// id = a;
+// id = b;
+// System.out.println(a + b);
+// System.out.println(a * b);
+// System.out.println(a / b);
+// System.out.println(a - b);
+// }
+
+// public static void main(String[] args) {
+// Calculator a = new Calculator(10, 2);
+// }
+// }
