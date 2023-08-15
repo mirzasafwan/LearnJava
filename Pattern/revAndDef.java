4cb7ba3947018fd5224f742850d53d4d8234760f
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-15 21:16:42 +0530

    Rhombus

--- Pattern/revAndDef.java
+++ Pattern/revAndDef.java
@@ -78,72 +78,46 @@
 // }
 
 // Kaaju Katli with 2 inner for loop and 1 outer For Loop
-
-// class revAndDef {
-// public static void main(String[] args) {
-// Scanner sc = new Scanner(System.in);
-// System.out.println("Enter the value of n");
-// int n = sc.nextInt();
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= n; j++) {
-// if (i == 1 || j == 1 || j <= n - i + 1) {
-// System.out.print(" *");
-// } else {
-// System.out.print(" ");
-// }
-// }
-// for (int j = 1; j <= n; j++) {
-// if (i == 1 || j == n || j >= i) {
-// System.out.print(" *");
-// } else {
-// System.out.print(" ");
-// }
-// }
-// System.out.println();
-// }
-// for (int i = 1; i <= n; i++) {
-// for (int j = 1; j <= n; j++) {
-// if (i == n || j == 1 || j <= i) {
-// System.out.print(" *");
-// } else {
-// System.out.print(" ");
-// }
-// }
-// for (int j = 1; j <= n; j++) {
-// if (i == n || j == n || j >= n - i + 1) {
-// System.out.print(" *");
-// } else {
-// System.out.print(" ");
-// }
-// }
-// System.out.println();
-// }
-
-// }
-// }
-
 import java.util.Scanner;
 
 class revAndDef {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n");
         int n = sc.nextInt();
-        if (n % 2 == 1) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (i == 0 || i == n - 1 || j <= n - i - 1 && i <= j
-                            || j >= n - i - 1 && i >= j) {
-                        System.out.print(" *");
-                    } else
-                        System.out.print("  ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || j <= n - i + 1) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
                 }
-                System.out.println();
             }
-        } else {
-            System.out.println("Odd Number supported only");
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == n || j >= i) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == n || j == 1 || j <= i) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == n || j == n || j >= n - i + 1) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
 
     }
