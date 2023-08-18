3805b81052a5999d237e19bfcf07b943f4c85848
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-18 22:18:41 +0530

    Naming Pattern

--- Pattern/NamingPattern.java
+++ Pattern/NamingPattern.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class NamingPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter The value of n");
-        // int n = sc.nextInt();
-        int n = 5;
         System.out.println("Enter Your name");
         String s = sc.next();
-
         String[] a = s.split("");
-        for (String character : a) {
 
-            switch (character.toUpperCase()) {
-                case "S":
-                    for (int i = 1; i <= n; i++) {
+        int n = 5; // Set the value of n
+
+        // Iterate through each row of the pattern
+        for (int i = 1; i <= n; i++) {
+            for (String character : a) { // Iterate through each character of the name
+                switch (character.toUpperCase()) {
+                    case "S":
+
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 && i <= (n / 2) + 1 || i == 1 || i == (n / 2) + 1 || j == n && i > n / 2
                                     || i == n) {
@@ -23,12 +23,10 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "A":
-                    for (int i = 1; i <= n; i++) {
+
+                        break;
+                    case "A":
+
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == 1 || j == n || i == (n / 2) + 1) {
                                 System.out.print(" *");
@@ -36,12 +34,9 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "F":
-                    for (int i = 1; i <= n; i++) {
+
+                        break;
+                    case "F":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == 1 || i == (n / 2) + 1) {
                                 System.out.print(" *");
@@ -49,12 +44,9 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "W":
-                    for (int i = 1; i <= n; i++) {
+
+                        break;
+                    case "W":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || j == n || i >= (n + 1) / 2 && j == n - i + 1
                                     || i >= (n + 1) / 2 && j == i) {
@@ -63,12 +55,8 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "N":
-                    for (int i = 1; i <= n; i++) {
+                        break;
+                    case "N":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == j || j == n) {
                                 System.out.print(" *");
@@ -76,12 +64,8 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "E":
-                    for (int i = 1; i <= n; i++) {
+                        break;
+                    case "E":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == 1 || i == n || i == (n / 2) + 1) {
                                 System.out.print(" *");
@@ -89,12 +73,8 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "M":
-                    for (int i = 1; i <= n; i++) {
+                        break;
+                    case "M":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || j == n || i <= (n + 1) / 2 && j == i
                                     || j >= (n + 1) / 2 && j == n - i + 1) {
@@ -103,30 +83,24 @@ public class NamingPattern {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                case "D":
-                    for (int i = 1; i <= n; i++) {
+                        break;
+                    case "D":
                         for (int j = 1; j <= n; j++) {
-                            if (j == 1 || i == 1 && j <= n - 1 || j == n && i != 1 && i != n - 1
+                            if (j == 1 || i == 1 && j <= n - 1 || j == n && i != 1 && i != n
                                     || i == n && j <= n - 1) {
                                 System.out.print(" *");
                             } else {
                                 System.out.print("  ");
                             }
                         }
-                        System.out.println();
-                    }
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("No such letter found");
+
+                        break;
+                    default:
+                        System.out.println("No such letter found");
+                }
+                System.out.print("  "); // Add a space between patterns
             }
-
+            System.out.println(); // Move to the next row after printing a row of patterns
         }
-
     }
-
 }
