767ca5d7710bb58d105bc4f51c3fd122bc8793fc
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-19 16:07:01 +0530

    Updating naming pattern

--- Pattern/NamingPattern.java
+++ Pattern/NamingPattern.java
@@ -14,12 +14,19 @@ public class NamingPattern {
             for (String character : a) { // Iterate through each character of the name
                 switch (character.toUpperCase()) {
                     case "A":
-                        for (int j = 1; j <= n; j++) {
-                            if (j == 1 || i == 1 || j == n || i == (n / 2) + 1) {
+                        for (int j = 1; j <= n - i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 1; j <= i; j++) {
+                            if (j == 1 || j >= i || i == (n / 2) + 1) {
                                 System.out.print(" *");
                             } else {
                                 System.out.print("  ");
                             }
+
+                        }
+                        for (int j = 1; j <= n - i; j++) {
+                            System.out.print(" ");
                         }
 
                         break;
@@ -43,15 +50,7 @@ public class NamingPattern {
                             }
                         }
                         break;
-                    case "H":
-                        for (int j = 1; j <= n; j++) {
-                            if (j == 1 || j == n || i == (n / 2) + 1) {
-                                System.out.print(" *");
-                            } else {
-                                System.out.print("  ");
-                            }
-                        }
-                        break;
+
                     case "D":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == 1 && j <= n - 1 || j == n && i != 1 && i != n
@@ -80,9 +79,59 @@ public class NamingPattern {
                             }
                         }
                         break;
+                    // G
+
+                    // H
+                    case "H":
+                        for (int j = 1; j <= n; j++) {
+                            if (j == 1 || j == n || i == (n / 2) + 1) {
+                                System.out.print(" *");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        break;
+
+                    // I
+                    case "I":
+                        for (int j = 1; j <= n; j++) {
+                            if (i == 1 || i == n || j == (n / 2) + 1) {
+                                System.out.print(" *");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        break;
+                    // J
+
+                    // K
+
+                    // L
+
                     case "L":
                         for (int j = 1; j <= n; j++) {
                             if (j == 1 || i == n) {
+                                System.out.print(" *");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        break;
+                    // L
+                    case "M":
+                        for (int j = 1; j <= n; j++) {
+                            if (j == 1 || j == n || i <= (n + 1) / 2 && j == i
+                                    || j >= (n + 1) / 2 && j == n - i + 1) {
+                                System.out.print(" *");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+                        break;
+                    // N
+                    case "N":
+                        for (int j = 1; j <= n; j++) {
+                            if (j == 1 || i == j || j == n) {
                                 System.out.print(" *");
                             } else {
                                 System.out.print("  ");
@@ -124,26 +173,6 @@ public class NamingPattern {
                     case "Z":
                         for (int j = 1; j <= n; j++) {
                             if (i == 1 || i == n || j == n - i + 1) {
-                                System.out.print(" *");
-                            } else {
-                                System.out.print("  ");
-                            }
-                        }
-                        break;
-                    case "M":
-                        for (int j = 1; j <= n; j++) {
-                            if (j == 1 || j == n || i <= (n + 1) / 2 && j == i
-                                    || j >= (n + 1) / 2 && j == n - i + 1) {
-                                System.out.print(" *");
-                            } else {
-                                System.out.print("  ");
-                            }
-                        }
-                        break;
-
-                    case "N":
-                        for (int j = 1; j <= n; j++) {
-                            if (j == 1 || i == j || j == n) {
                                 System.out.print(" *");
                             } else {
                                 System.out.print("  ");
