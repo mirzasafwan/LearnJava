cd957743d047e10196fed691e1c71e0a76ba9231
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-08-21 13:43:16 +0530

    Updating while with for Loop

--- Pattern/OneForPattern.java
+++ Pattern/OneForPattern.java
@@ -1,14 +1,27 @@
+// public class OneForPattern {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         int i = 0;
+
+//         while (i < n * n) {
+//             System.out.print("* ");
+//             if ((i + 1) % n == 0) {
+//                 System.out.println();
+//             }
+//             i++;
+//         }
+//     }
+// }
 public class OneForPattern {
     public static void main(String[] args) {
         int n = 5;
-        int i = 0;
 
-        while (i < n * n) {
-            System.out.print("* ");
-            if ((i + 1) % n == 0) {
+        for (int i = 1; i <= n * n; i++) {
+            System.out.print(" *");
+            if (i % n == 0) {
                 System.out.println();
             }
-            i++;
         }
+
     }
 }
