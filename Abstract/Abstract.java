a38a333bc565a938751643c860cdb03d3d5a0aa5
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-09-02 16:47:15 +0530

    Abstract

--- Abstract/Abstract.java
+++ Abstract/Abstract.java
@@ -1,50 +1,71 @@
-abstract class A {
-    public abstract int m1();
+// abstract class A {
+// public abstract int m1();
 
-    public abstract int m2(int x);
+// public abstract int m2(int x);
 
-    int y;
-    int x = 5;
+// int y;
+// int x = 5;
 
-    int m4(int x, int y) {
-        this.x = x;
+// int m4(int x, int y) {
+// this.x = x;
 
-        System.out.println(x + " " + this.y);
-        return 'c';
-    }
+// System.out.println(x + " " + this.y);
+// return 'c';
+// }
 
-}
+// }
 
-class B extends A {
+// class B extends A {
 
-    B(float y) {
-        y = 10;
-        System.out.println(super.y);
+// B(float y) {
+// y = 10;
+// System.out.println(super.y);
 
-    }
+// }
 
-    {
-        System.out.println("In IIB of B " + y);
-    }
+// {
+// System.out.println("In IIB of B " + y);
+// }
 
-    public int m1() {
-        System.out.println("m1");
-        return 'c';
-    }
+// public int m1() {
+// System.out.println("m1");
+// return 'c';
+// }
 
-    public int m2(int x) {
-        m4(10, 2);
-        System.out.println("m2");
-        return 1;
-    }
+// public int m2(int x) {
+// m4(10, 2);
+// System.out.println("m2");
+// return 1;
+// }
 
-}
+// }
 
-class Abstract {
-    public static void main(String[] args) {
-        B b = new B(1.0f);
-        b.m1();
-        b.m4(10, 20);
-        b.m2(10);
-    }
-}
+// class Abstract {
+// public static void main(String[] args) {
+// B b = new B(1.0f);
+// b.m1();
+// b.m4(10, 20);
+// b.m2(10);
+// }
+// }
+
+// class Abstract {
+// static Abstract obj = new Abstract();
+
+// static {
+// System.out.println("Static");
+// }
+
+// void m1() {
+// System.out.println("Method");
+// }
+
+// {
+// System.out.println("IIB");
+// }
+
+// public static void main(String[] args) {
+// // Abstract obj = new Abstract();
+// obj.m1();
+// }
+// }
---
+++ Abstract/Interface.java
@@ -0,0 +1,19 @@
+class Interface {
+    // class A {
+    // interface B {
+    // void m1();
+    // }
+    // }
+
+    // class B extends A implements A.B {
+    // public void m1() {
+    // System.out.println("Class B Interface A");
+    // }
+    // }
+
+    public static void main(String[] args) {
+        int a = 69;
+        char b = (char) a;
+        System.out.println(b);
+    }
+}
