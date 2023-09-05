dad7fc6943bc59481572e89b27b5ee7f8e653219
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-09-05 16:57:26 +0530

    Adding a Abstract

--- Abstract/Interface.java
+++ Abstract/Interface.java
@@ -1,28 +1,25 @@
 interface A {
     int a = 1;
 
-    void m1();
+    int m1();
 }
 
 interface B {
     int b = 2;
 
-    void m1();
+    int m1();
 }
 
 public class Interface implements A, B {
 
     @Override
-    public void m1() {
-        System.out.println(A.a);
-        System.out.println(B.b);
-        // TODO Auto-generated method stub
-
+    public int m1() {
+        return b + a;
     }
 
     public static void main(String[] args) {
         Interface a = new Interface();
-        a.m1();
+        System.out.println(a.m1());
     }
 
 }
